0fa390244a2b282141f687d4abb1d79784fff589
ashutoshkrchaubey <ashutoshkrchaubey@gmail.com>
2021-03-14 22:18:29 +0530

    Finished authentication setup

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,8 @@ defmodule DiscussWeb.TopicController do
 
   alias Discuss.Topics
 
+  plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
   def index(conn, _params) do
     topics = Repo.all(Topics)
     render conn, "index.html", topics: topics
